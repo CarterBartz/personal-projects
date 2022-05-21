e67e45578951ff358a50dbdea05a9b0f405ba875
Carter Bartz <carter.bartz@icloud.com>
2022-05-20 21:33:47 -0500

    First Push

---
+++ src/src/com/bank_account/Main.java
@@ -0,0 +1,10 @@
+package com.bank_account;
+
+public class Main {
+
+    // use "psvm" for a shortcut
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
